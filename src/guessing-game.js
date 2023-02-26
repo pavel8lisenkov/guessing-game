9b75b5e984ad61da551bd8ca38bb6d6c14ed2f6f
Pasha <pavel8lisenkov@yandex.ru>
2023-02-27 02:49:51 +0400

    feat: tasc complited

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this.result;
+    }
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
-
+      this.result = Math.ceil((this.max - this.min) / 2 + this.min);
+      return this.result;
     }
 
     lower() {
-
+      this.max = this.result - 1;
     }
 
     greater() {
-
+      this.min = this.result + 1;
     }
 }
 
